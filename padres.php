247ea8c863c80dcf3bce8b3fb45193264a214ddf
Jesus Pion <jesuspiont@gmail.com>
2019-11-20 20:48:15 -0500

    Added changes to distribution in pisoton page

--- padres.php
+++ padres.php
@@ -77,13 +77,13 @@
     </div>
 
     <div class="information-resources pt-3">
-      <div class="articles-links py-2 py-lg-5">
-          <a class="mb-5 mr-lg-4 text-lg-right" href="articles.php"><img src="img/padres/elementos-39.png" alt="Nos emociona educar"></a>
-          <a class="mb-3 text-lg-left" href="/articles.php"><img src="img/padres/elementos-40.png" alt="Razones y corazones"></a>
+      <div class="articles-links py-2 py-lg-3">
+          <a class="mb-3 mr-lg-4 text-lg-right" href="articles.php"><img src="img/padres/elementos-39.png" alt="Nos emociona educar"></a>
+          <a class="mb-5 text-lg-left" href="/articles.php"><img src="img/padres/elementos-40.png" alt="Razones y corazones"></a>
       </div>
       <div class="images-to-color">
         <div class="info">
-          <h2 class="mb-3"> Colorea en familia</h2>
+          <h2 class="pt-0 mb-3"> Colorea en familia</h2>
           <p class="mb-4">Conoce a los personajes de la divertida serie Las Aventuras de Ugo y Ema, y junto a tus hijos, colorea en casa</p>
         </div>
         <div id="downloader">
